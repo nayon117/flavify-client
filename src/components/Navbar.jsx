7d9c9fa45cc19e1f82a741a04b746ae88fada554
nayon117 <hasibulhasannayon260@gmail.com>
2023-12-19 11:25:29 +0600

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,122 @@
-
+import { Link } from "react-router-dom";
+import { IoIosMenu } from "react-icons/io";
+import logo from "../assets/logo.png";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Menu</summary>
+          <ul className="p-2">
+            <li>
+              <a>All</a>
+            </li>
+            <li>
+              <a>Salad</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li tabIndex={0}>
+        <details>
+          <summary>Services</summary>
+          <ul className="p-2">
+            <li>
+              <a>Order Online</a>
+            </li>
+            <li>
+              <a>Table Booking</a>
+            </li>
+            <li>
+              <a>Order Tracking</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Offers</a>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <h2>This is Navbar</h2>
-    </div>
+    <header className="max-w-screen-xl mx-auto">
+      <div className="navbar px-4 md:px-8 lg:px-16 ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <IoIosMenu className="text-2xl" />
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navItems}
+            </ul>
+          </div>
+          <Link href="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
+        </div>
+        <div className="navbar-end">
+          {/* search icons */}
+          <button className="btn btn-ghost btn-circle hidden lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          {/* cartbtn */}
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle mr-3 hidden lg:flex items-center justify-center"
+          >
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          {/* contact btn */}
+          <a className="btn bg-green flex items-center gap-2 text-white rounded-full px-6">
+            <FaPhoneAlt /> Contact
+          </a>
+        </div>
+      </div>
+    </header>
   );
 };
 export default Navbar;
