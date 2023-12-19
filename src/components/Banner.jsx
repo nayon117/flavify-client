53809353e9466a6979e492d906059d4e516e9d3c
nayon117 <hasibulhasannayon260@gmail.com>
2023-12-19 12:09:07 +0600

    create banner part

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import bannerImg from '../assets/images/home/banner.png'
+const Banner = () => {
+  return (
+      <div className="section-container flex flex-col md:flex-row items-center ">
+          {/* text */}
+          <div className="flex-1 space-y-6">
+              <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-snug">Indulge in Scrumptious <span className="text-green">Delights!</span></h2>
+              <p>Where Each Plate Unfolds a Tale of Culinary Mastery and Passionate Craftsmanship.</p>
+              <button className="btn bg-green text-white py-3 px-4 rounded-full">Order Now</button>
+          </div>
+          {/* image */}
+          <div className="flex-1">
+              <img src={bannerImg} alt="" />
+      </div>
+    </div>
+  );
+};
+export default Banner;
