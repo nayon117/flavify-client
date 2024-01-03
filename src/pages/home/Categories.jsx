216b2babd687121c2d193c695051c262806c2339
nayon117 <hasibul.nayon1@gmail.com>
2024-01-03 09:42:05 +0600

    create category section

---
+++ src/pages/home/Categories.jsx
@@ -0,0 +1,40 @@
+import img1 from '../../assets/images/home/category/img1.png'
+import img2 from '../../assets/images/home/category/img2.png'
+import img3 from '../../assets/images/home/category/img3.png'
+import img4 from '../../assets/images/home/category/img4.png'
+const Categories = () => {
+    const categoriesItems = [
+        {id:1, title:"Main Dish", desc:"86 dish", img:img1},
+        {id:2, title:"Breakfast", desc:"(12 breakfast)", img:img2},
+        {id:3, title:"Dessert", desc:"(48 dish)", img:img3},
+        {id:4, title:"Browse All", desc:"(1275 dish)", img:img4}
+    ]
+  return (
+    <div className="section-container py-16">
+      <div className="text-center">
+        <h2 className="subtitle">
+          Customer Favorites
+        </h2>
+        <h2 className="title">
+          Popular Categories
+        </h2>
+          </div>
+
+          {/* category items */}
+      <div className='flex flex-col md:flex-row gap-8 flex-wrap justify-around items-center mt-12'>
+        {
+          categoriesItems?.map((item, idx) => <div key={idx} className='shadow-md rounded-md bg-white py-6 px-5 mx-auto text-center cursor-pointer hover:-translate-y-4 duration-300 transition-all'>
+            <div>
+              <img src={item.img} alt="" className='p-5 bg-green rounded-md w-28 h-28'/>
+            </div>
+            <div className='mt-5 space-y-1'>
+              <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+            </div>
+          </div>)
+        }
+      </div>
+    </div>
+  );
+};
+export default Categories;
