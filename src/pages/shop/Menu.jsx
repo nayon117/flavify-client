68a1686e04e2a79f0b3017d01fe6c4312d9b67fb
nayon117 <hasibul.nayon1@gmail.com>
2024-01-13 11:35:48 +0600

    implement sorting funcitonality

--- src/pages/shop/Menu.jsx
+++ src/pages/shop/Menu.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Cards from "../../components/Cards";
+import { FaFilter } from "react-icons/fa";
 
 const Menu = () => {
     const [menu, setMenu] = useState([])
@@ -42,7 +43,7 @@ const Menu = () => {
     const handleSortChange = (option) => {
         setSortOption(option)
 
-      let sortedItems = [...filterItems]
+      let sortedItems = [...filteredItems]
       
       // logic
       switch(option) {
@@ -93,12 +94,44 @@ const Menu = () => {
         <div>
           {/* all category data */}
           <div className="flex justify-start items-center gap-8 flex-wrap font-medium my-3">
-            <button onClick={showAll}>All</button>
-            <button onClick={()=>filterItems("salad")}>Salad</button>
-            <button onClick={()=>filterItems("pizza")}>Pizza</button>
-            <button onClick={()=>filterItems("soup")}>Soups</button>
-            <button onClick={()=>filterItems("dessert")}>Dessert</button>
-            <button onClick={()=>filterItems("drinks")}>Drinks</button>
+            <button onClick={showAll}
+            className={selectCategory==="all"? "active" : ""}
+            >All</button>
+            <button onClick={() => filterItems("salad")}
+            className={selectCategory==="salad"? "active" : ""}
+            >Salad</button>
+            <button onClick={() => filterItems("pizza")}
+            className={selectCategory==="pizza"? "active" : ""}
+            >Pizza</button>
+            <button onClick={() => filterItems("soup")}
+            className={selectCategory==="soup"? "active" : ""}
+            >Soups</button>
+            <button onClick={() => filterItems("dessert")}
+            className={selectCategory==="dessert"? "active" : ""}
+            >Dessert</button>
+            <button onClick={() => filterItems("drinks")}
+            className={selectCategory==="drinks"? "active" : ""}
+            >Drinks</button>
+          </div>
+
+          {/* sorting based on filtering */}
+          <div>
+            <div className="bg-black p-2">
+              <FaFilter className="h-4 w-4 text-white"/>
+            </div>
+
+            {/* sorting options */}
+            <select name="sort" id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-2 py-1 rounded-sm"
+            >
+              <option value="default">Default</option>
+              <option value="A-Z">A-Z</option>
+              <option value="Z-A">Z-A</option>
+              <option value="low-to-high">low-to-high</option>
+              <option value="high-to-low">high-to-low</option>
+            </select>
           </div>
         </div>
         
